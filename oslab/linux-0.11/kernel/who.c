e900b7061fbc1e3ea84a3824eacd9eb56c3bd445
derobukal <derobukal@gmail.com>
2016-08-22 13:32:35 +0000

    something wrong in lab2

---
+++ oslab/linux-0.11/kernel/who.c
@@ -0,0 +1,13 @@
+#define __LIBRARY__
+
+#include <unistd.h>
+
+int sys_iam(const char* name){
+    return 0;
+}
+
+int sys_whoami(char* name, unsigned int size){
+    printk("11111111111111111");
+    return 0;
+}
+
