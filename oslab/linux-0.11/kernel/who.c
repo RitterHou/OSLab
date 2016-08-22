3754a4de29b6d250f65c5df3d1bd290e09733c71
derobukal <derobukal@gmail.com>
2016-08-22 15:07:48 +0000

    lab2 complete

--- oslab/linux-0.11/kernel/who.c
+++ oslab/linux-0.11/kernel/who.c
@@ -1,13 +1,37 @@
 #define __LIBRARY__
 
 #include <unistd.h>
+#include <errno.h>
+#include <asm/segment.h>
+
+static char myname[24] = {0};
 
 int sys_iam(const char* name){
-    return 0;
+    int i= 0;
+    while(get_fs_byte(name + i)!='\0')
+	i++;
+    if(i>23){
+        return -EINVAL;
+    }
+    printk("%d]\n",i);
+    i = 0;
+    while(myname[i] = get_fs_byte(name + i)!='\0'){
+        i++;
+    }
+    return i;
 }
 
 int sys_whoami(char* name, unsigned int size){
-    printk("11111111111111111");
-    return 0;
+    int i = 0;
+    while(myname[i] != '\0')
+        i++;
+    if(size<i)
+        return -1;
+    i = 0;
+    while(myname[i] != '\0'){
+        put_fs_byte(myname[i],(name+i));
+        i++;
+    }
+    return i;
 }
 
